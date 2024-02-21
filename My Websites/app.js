81097624b156f5fa4258adfdaf84d56d296c797b
Jubayer Fahim <156297223+jubayerfahim2002@users.noreply.github.com>
2024-02-21 20:16:36 +0600

    Update app.js

--- My Websites/app.js
+++ My Websites/app.js
@@ -26,23 +26,6 @@
 
 let slides = document.querySelectorAll('.slid-container');
 let index = 0;
-
- setInterval(()=>{
-     if(slides[0].classList.contains('active')){
-        slides[1].classList.add('active')
-         slides[0].classList.remove('active')
-     }else if(slides[1].classList.contains('active')){
-       slides[2].classList.add('active')
-        slides[1].classList.remove('active')
-     }else if(slides[2].classList.contains('active')){
-        slides[3].classList.add('active')
-         slides[2].classList.remove('active')
-    }else if(slides[3].classList.contains('active')){
-        slides[0].classList.add('active')
-        slides[3].classList.remove('active')
-    }
- },5000)
-
     function next(){
         slides[index].classList.remove('active');
         index = (index + 1) % slides.length;
